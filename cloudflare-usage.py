f013133f0255543f27f483609359518f20c7ae3e
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-04-18 16:48:33 -0400

    Sort alphabetically Also, minor restructuring

--- cloudflare-usage.py
+++ cloudflare-usage.py
@@ -5,15 +5,19 @@ from tranco import Tranco
 from tqdm import tqdm
 
 NUM_DOMAINS = 250
-UA_CHOICES = ["Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0","Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0","Microsoft Edge Legacy User-Agent string: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70..3538.102 Safari/537.36 Edge/18.19582"]
+UA_CHOICES = ["Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0","Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0","Microsoft Edge Legacy User-Agent string: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70..3538.102 Safari/537.36 Edge/18.19582","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0"]
 REQUEST_TIMEOUT = 45
 RETRY_ENABLED = True
 REPORT_FILE = "report.md"
 DOMAINS_FILE = "domains.txt"
 IPS_FILE = "ips.txt"
 TOPHASH_FILE = "topdomains_hash.txt"
+SORT_DOMAINS = True
+CACHE_LIST = False # pointless with GitHub actions
+REQUEST_METHOD = "HEAD" # HEAD gives us what we need
+PROGRESS_BAR_ENABLED = "--noprogress" not in sys.argv # read from sys.argv, overwrite to always enable/disable
 
-t = Tranco(cache=False)
+trancolist = Tranco(cache=CACHE_LIST)
 latest_list = t.list()
 topdomains = latest_list.top(NUM_DOMAINS)
 
@@ -23,10 +27,10 @@ seenips = []
 def hascloudflare(url):
 	try:
 		headers = {"user-agent":random.choice(UA_CHOICES)}
-		r = requests.head(url,timeout=REQUEST_TIMEOUT,headers=headers)
+		r = requests.requests(url=url,method=REQUEST_METHOD,timeout=REQUEST_TIMEOUT,headers=headers)
 		if "Server" in r.headers:
 			return r.headers["Server"] == "cloudflare"
-	except Exception as err:
+	except Exception:
 		return None
 	return False
 def saveip(domain):
@@ -65,10 +69,10 @@ Domains using CloudFlare:
 	tophash.write(hashlib.sha256(";".join(topdomains).encode()).hexdigest())
 	tophash.close()
 
-if "--noprogress" in sys.argv:
-	domainsarray = topdomains
-else:
+if PROGRESS_BAR_ENABLED:
 	domainsarray = tqdm(topdomains)
+else:
+	domainsarray = topdomains
 
 for d in domainsarray:
 	httptestresult = hascloudflare(f"http://{d}")
@@ -82,6 +86,9 @@ for d in domainsarray:
 			saveip(d)
 		elif httpstestresult == None:
 			erroredout += 1
+
+if SORT_DOMAINS:
+	hascloud.sort()
 
 savedomains()
 saveips()
