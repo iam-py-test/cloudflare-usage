e6965ffdbb8f093bf547e4279485b2fc440a6cc8
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-10-02 16:00:08 -0400

    Scan for CloudFront, Akamai, and Sucuri (part 2) Kill the domain_ip_map once and forall

--- cloudflare-usage.py
+++ cloudflare-usage.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 NUM_DOMAINS = 1000
 UA_CHOICES = ["Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0","Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0","Microsoft Edge Legacy User-Agent string: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70..3538.102 Safari/537.36 Edge/18.19582","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0", 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188', "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"]
-REQUEST_TIMEOUT = 40
+REQUEST_TIMEOUT = 50
 RETRY_ENABLED = True
 REPORT_FILE = "report.md"
 DOMAINS_FILE = "domains.txt"
@@ -19,8 +19,6 @@ REQUEST_METHOD = "HEAD" # HEAD gives us what we need
 PROGRESS_BAR_ENABLED = "--noprogress" not in sys.argv # read from sys.argv, overwrite to always enable/disable
 DEBUG = False # set to True when testing
 MAX_THREADS = 95
-
-domain_ip_map = {}
 
 def debugmsg(msg,data="No data!"):
 	if DEBUG:
@@ -119,15 +117,11 @@ def savedomains():
 def saveips():
 	for p in seenips:
 		ipsfile = open(f"{p}_ips.txt",'w',encoding="UTF-8")
-		ipsfile.write("\n".join(seenips))
+		ipsfile.write("\n".join(seenips[p]))
 		ipsfile.close()
 def savecnames():
 	ipsfile = open("cnames.txt",'w',encoding="UTF-8")
 	ipsfile.write("\n".join(cnames))
-	ipsfile.close()
-def savemap():
-	ipsfile = open("domain_ip_map.json",'w',encoding="UTF-8")
-	ipsfile.write(json.dumps(domain_ip_map))
 	ipsfile.close()
 def savereport():
 	reportfile = open(REPORT_FILE,'w')
@@ -153,7 +147,6 @@ else:
 	domainsarray = topdomains
 
 def checkdomain(d):
-	global domain_ip_map
 	global erroredout
 	global hascloud
 	global hascloudfront
@@ -209,5 +202,4 @@ if SORT_DOMAINS:
 savedomains()
 saveips()
 savereport()
-savemap()
 savecnames()
